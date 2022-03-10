acd9ca582946bc660d500892626ed0ecc8d446ef
Luca Heiler <luca.h41@gso.schule.koeln>
2022-03-10 10:14:59 +0100

    Projektdateien hinzufügen.

---
+++ SAS-Grundlagen GSO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SAS_Grundlagen_GSO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello ja World!");
+        }
+    }
+}
